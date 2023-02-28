2423079fae2a3a3c5dc34b823e2d29a57782ef2b
LeThanhNhan <nhanlethanhit@gmail.com>
2023-02-28 16:23:11 +0700

    fix:deploy

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const corsOptions = {
     "http://localhost:3000",
     // "http://127.0.0.1",
     // "http://104.142.122.231",
+    "https://frontend-test-7xxhwshwb-nhanlethanhit.vercel.app"
   ],
   credentials: true,
 };
